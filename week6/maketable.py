7958a94c4fa65c869148310969507cbdf4e4819b
Sakphichet Kliangphrom <sakphichet.k@kkumail.com>
2021-02-13 14:40:54 +0700

    submit

--- week6/maketable.py
+++ week6/maketable.py
@@ -1,7 +1,7 @@
 import sqlite3
 conn = sqlite3.connect (r"C:\Users\acer\Documents\Sakphichet_Python\week6\work6.db")
 c = conn.cursor()
-c.execute ('''CREATE TABLE data (ID integer PRIMARY KEY NOT NULL,
+c.execute ('''CREATE TABLE data (ID integer PRIMARY KEY AUTOINCREMENT,
     Name varchar(50) NOT NULL,
     Sex varchar(10) NOT NULL,
     Age varchar(10) NOT NULL,
